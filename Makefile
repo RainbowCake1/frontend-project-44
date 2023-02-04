fe2caaa4b92a952480333f207c5231f25cff7f1a
RainbowCake1 <rainbowuran@gmail.com>
2023-02-04 20:38:17 +0300

    step6

--- Makefile
+++ Makefile
@@ -8,3 +8,5 @@ makelint:
 	npx eslint .
 brain-even:
 	node bin/brain-even.js
+brain-calc:
+	node bin/brain-calc.js
---
+++ src/calc-game.js
@@ -0,0 +1,52 @@
+import readlineSync from 'readline-sync';
+
+function randNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function randOperation() {
+ const rnd = Math.round(Math.random() * 2 + 1);
+ if (rnd === 1) {
+  return '-'
+ } else if ( rnd === 2) {
+  return '+'
+ } else {
+  return '*'
+ }
+}
+
+function calc(num1,num2,oper){
+  switch(oper) {
+    case '-': return num1 - num2;
+    case '+': return num1 + num2;
+    case '*': return num1 * num2;
+  }
+}
+
+function game(userName) {
+  console.log('What is the result of the expression?');
+  for (let i = 0; i < 3; i++ ) {
+    const num1 = randNum(16);
+    const num2 = randNum(16);
+    const opr = randOperation()
+    console.log(`'Question:'${num1} ${opr} ${num2}`);
+   const rightAnswer = calc(num1,num2,opr)
+    const ques = readlineSync.question('You answer:');
+
+    if (ques == rightAnswer) {
+      console.log('Correct!');
+    } else {
+    console.log(`"${ques}" is wrong anser ;(. Correct answer was ${rightAnswer}. \nLet\'s try again, ${userName}`);
+     return
+    }
+}
+console.log(`Congratulations, ${userName}!`)
+}
+
+function gameCalc(){
+  console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hi ' + `${userName}` + '!');
+    game(userName);
+}
+export default gameCalc

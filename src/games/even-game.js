47379bf23a7f5a82356e155f55c536b73d1bd98c
RainbowCake1 <rainbowuran@gmail.com>
2023-03-05 22:20:57 +0300

    fix brain-even

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -3,7 +3,7 @@ import randNum from "../tools/randomnumbers.js";
 
 const isEven = (number) => (number % 2 === 0) ? 'yes' : 'no';
 
-const task = 'Answer "yes" if given number is even. Otherwise answer "no".';
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const game = () => {
     const number = randNum(15);

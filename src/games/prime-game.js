f24fe0a5e3b45d5f756f224b7f7ecb6177bc23da
RainbowCake1 <rainbowuran@gmail.com>
2023-03-11 16:46:07 +0300

    primegame

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,16 +4,22 @@ import randomInter from '../tools/randint.js';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(num) {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return 'no';
+  if (num === 2) {
+    return true;
   }
-  return 'yes';
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      false;
+    }
+    return true;
+  }
 }
 
 function game() {
   const num = randomInter(2, 16);
   const question = `${num}`;
-  const answer = isPrime(num);
+  const answer = isPrime(num) ? 'yes' : 'no';
   return [question, answer];
 }
-export default play(task, game);
+const result = play(task, game);
+export default result;
